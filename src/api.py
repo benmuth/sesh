3ffd45f6e957e6bb17aeb3b3bfe090bdc8073504
Ben Muthalaly <benmuthalaly@gmail.com>
2023-11-12 04:18:22 -0600

    Add function to get user projects from API

--- src/api.py
+++ src/api.py
@@ -1,17 +1,35 @@
 import requests
 import os
 from requests.auth import HTTPBasicAuth
+from pprint import pprint
 
-# curl -u <token>:api_token
-# token =
-username = os.environ.get("TOGGL")
+api_token = os.environ.get("TOGGL")
 toggl_url = "https://api.track.toggl.com/api/v9/me"
 
 
-def make_auth_request():
-    if username:
-        response = requests.get(toggl_url, auth=HTTPBasicAuth(username, "api_token"))
-        print("response: status ", response.status_code)
+# curl -u <token>:api_token
+def get_user_info(api_token):
+    """
+    returns details for the user with the given api_token as JSON.
+    """
+    response = requests.get(toggl_url, auth=HTTPBasicAuth(api_token, "api_token"))
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Unauthorized
+
+
+def get_user_projects(api_token):
+    """
+    returns details for the user with the given api_token as JSON.
+    """
+    response = requests.get(
+        toggl_url + "/projects",
+        headers={"content-type": "application/json"},
+        auth=HTTPBasicAuth(api_token, "api_token"),
+    )
+    if response.status_code == 200:
+        return response.json()
     else:
         raise Unauthorized
 
@@ -22,6 +40,7 @@ class Unauthorized(Exception):
 
 if __name__ == "__main__":
     try:
-        make_auth_request()
+        pprint(get_user_info(api_token))
+        pprint(get_user_projects(api_token))
     except Unauthorized:
         print("Unauthorized")
