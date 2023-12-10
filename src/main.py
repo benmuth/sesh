09ed4e65ebb4c9fc654f6851900d25daefc2ad9d
Ben Muthalaly <benmuthalaly@gmail.com>
2023-12-10 07:44:21 -0600

    Move code into single file

---
+++ src/main.py
@@ -0,0 +1,351 @@
+#!/usr/bin/env python3
+import argparse
+from datetime import datetime, timezone
+
+# from . import core
+import requests
+import os
+from requests.auth import HTTPBasicAuth
+
+import json
+
+
+class NoData(Exception):
+    pass
+
+
+# ------------------------------------
+# API
+# ------------------------------------
+
+# TODO: make this not hardcoded
+workspace_id = 5992965
+
+toggl_url = "https://api.track.toggl.com/api/v9/me"
+
+api_token: str | None = os.environ.get("TOGGL")
+if not api_token:
+    raise Exception("No toggle API token found")
+
+
+# curl -u <token>:api_token
+def get_user_info(api_token: str = api_token):
+    """
+    returns details for the user with the given api_token as JSON.
+    """
+    response = requests.get(toggl_url, auth=HTTPBasicAuth(api_token, "api_token"))
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Unauthorized
+
+
+def get_user_projects(api_token: str = api_token):
+    """
+    returns the projects of the user with the given api_token as JSON.
+    """
+    response = requests.get(
+        toggl_url + "/projects",
+        headers={"content-type": "application/json"},
+        auth=HTTPBasicAuth(api_token, "api_token"),
+    )
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Unauthorized
+
+
+def get_user_tags(api_token: str = api_token):
+    """
+    Gets project names and ids
+    the format is a list of dicts like this
+            {'at': '2022-08-26T20:33:32.262363Z',
+              'id': 10950283,
+              'name': 'Tour of Go',
+              'workspace_id': 5992965}
+    """
+    response = requests.get(
+        f"https://api.track.toggl.com/api/v9/workspaces/{workspace_id}/tags",
+        headers={"content-type": "application/json"},
+        auth=HTTPBasicAuth(api_token, "api_token"),
+    )
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Unauthorized
+
+
+def get_running_timer(api_token: str = api_token) -> dict[str, str]:
+    url = toggl_url + "/time_entries/current"
+    response = requests.get(
+        url,
+        headers={"content-type": "application/json"},
+        auth=HTTPBasicAuth(api_token, "api_token"),
+    )
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Unauthorized
+
+
+def stop_timer(time_entry_id: str, workspace_id: str):
+    url = f"https://api.track.toggl.com/api/v9/workspaces/{workspace_id}/time_entries/{time_entry_id}/stop"
+    response = requests.patch(
+        url,
+        headers={
+            "content-type": "application/json",
+        },
+        auth=HTTPBasicAuth(api_token, "api_token"),
+    )
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Unauthorized
+
+
+def start_timer(project_name: str, project_id: int, start_time: str, tag_id: int):
+    print("start_time:", start_time)
+    if not project_id or not start_time or not workspace_id:
+        raise NoData
+    payload = {
+        "created_with": "sesh",
+        "duration": -1,
+        "project_id": project_id,
+        "start": start_time,
+        "tag_action": "add",
+        "tag_ids": [tag_id],
+        "tags": [project_name],
+        "workspace_id": workspace_id,
+    }
+    response = requests.post(
+        f"https://api.track.toggl.com/api/v9/workspaces/{workspace_id}/time_entries",
+        json=payload,
+        headers={"content-type": "application/json"},
+        auth=HTTPBasicAuth(api_token, "api_token"),
+    )
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(response.reason)
+        print(response.text)
+        print(response.request.url)
+        print(response.request.headers)
+        # print(response.request.body.decode("utf-8"))
+        # for c in response.request.body:
+        #     print(str(c))
+
+
+class Unauthorized(Exception):
+    pass
+
+
+# ------------------------------------
+# DATA
+# ------------------------------------
+
+# TODO: hook up to SQLite
+
+# TODO: create a class that holds all user data (including a dict mapping project names to IDs)
+
+
+def sync_projects_with_toggl():
+    """
+    Writes a list of the user's projects to 'data/projects.json'.
+    The list is sorted by user time recorded.
+    """
+    project_list = get_user_projects()
+    sorted_project_list = sorted(
+        project_list, key=lambda x: x["actual_seconds"], reverse=True
+    )
+    # print(sorted_project_list)
+    project_properties = ["name", "color", "id"]
+    projects = [{k: d[k] for k in project_properties} for d in sorted_project_list]
+    print("PROJECT DATA RECEIVED FROM TOGGL")
+    print(json.dumps(projects, indent=2))
+
+    with open("data/projects.json", "wt") as f:
+        f.write(json.dumps(projects))
+
+
+def sync_tags_with_toggl():
+    """
+    Writes a list of the user's tags to 'data/tags.json'.
+    """
+    tag_list = get_user_tags()
+    tag_properties = ["name", "id"]
+    tags = [{k: d[k] for k in tag_properties} for d in tag_list]
+    print("TAG DATA RECEIVED FROM TOGGL")
+    print(json.dumps(tags, indent=2))
+
+    with open("data/tags.json", "wt") as f:
+        f.write(json.dumps(tags))
+
+
+class UserData:
+    def __init__(
+        self
+    ):  # TODO: handle case where tags and projects files are empty (sync with toggl)
+        self.tags = read_data("tags.json")
+        self.projects = read_data("projects.json")
+
+
+# TODO: separate stored data by user
+def read_data(
+    data_file: str, data_dir: str = "/home/ben/code/python/sesh/data/"
+) -> list[dict[str, str]] | None:
+    """Reads the given data file if it exists and returns it as a dictionary"""
+    valid_data_files = ("projects.json", "tags.json")
+    if data_file in valid_data_files:
+        with open(os.path.join(data_dir + data_file), "rt") as f:
+            data: list[dict[str, str]] = json.JSONDecoder().decode(f.read())
+            return data
+    else:
+        print(f"given data file {data_file} not found")
+        return None
+
+
+data_dir = "/home/ben/code/python/sesh/data/"
+
+# for datum in read_data(data_dir=data_dir):
+# print(datum["name"])
+
+# sync_projects_with_toggl(api_token=api_token)
+
+
+# ------------------------------------
+# CLI
+# ------------------------------------
+
+
+def timer(args: argparse.Namespace):
+    if args.project in args.projects:
+        print(args.project)
+    else:
+        print(
+            f"timer {args.project} not found in list of projects. Please choose from {args.projects}"
+        )
+
+
+def stop(args: list[str]):
+    running_timer = get_running_timer()
+    time_entry_id = running_timer["id"]
+    workspace_id = running_timer["workspace_id"]
+    timer_name = running_timer["tags"][0]
+    print(time_entry_id, workspace_id, timer_name)
+    response = stop_timer(time_entry_id=time_entry_id, workspace_id=workspace_id)
+    print("stop response: %s" % response)
+    print("stopped %s" % timer_name)
+
+
+def start(args: argparse.Namespace):
+    if args.project in args.projects and args.project in args.tags:
+        project_id = None
+        print(args.project)
+        for project in args.user_data.projects:
+            if project["name"] == args.project:
+                project_id = project["id"]
+
+        tag_id = None
+        for tag in args.user_data.tags:
+            if tag["name"] == args.project:
+                tag_id = tag["id"]
+
+        now: str = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+        if project_id is None:
+            raise NoData
+        if tag_id is None:
+            raise NoData
+        print("start", args.project)
+        start_timer(
+            project_name=args.project,
+            project_id=project_id,
+            start_time=now,
+            tag_id=tag_id,
+        )
+    else:
+        print(f"timer {args.project} not found in list of projects")
+        print("projects:", args.projects)
+        print("tags:", args.tags)
+        return
+
+
+def sync(args: list[str]):
+    sync_projects_with_toggl()
+    sync_tags_with_toggl()
+
+
+def show_running_timer(args: argparse.Namespace):
+    response = get_running_timer()
+    if response:
+        print(response["tags"][0])
+    else:
+        print("No timer running")
+
+
+def main():
+    stored_projects: list[dict[str, str]] | None = read_data("projects.json")
+    if not stored_projects:
+        raise NoData
+    projects: list[str] = [x["name"] for x in stored_projects]
+
+    stored_tags: list[dict[str, str]] | None = read_data("tags.json")
+    if not stored_tags:
+        raise NoData
+    tags: list[str] = [x["name"] for x in stored_tags]
+
+    user_data = UserData()
+
+    parser = argparse.ArgumentParser(
+        description="Control Toggl Track timers from the command line"
+    )
+    parser.add_argument(
+        "-r",
+        "--running",
+        help="display the currently running timer",
+        action="store_true",
+        required=False,
+    )
+
+    parser.add_argument(
+        "-l",
+        "--list",
+        help="display all projects",
+        action="store_true",
+        required=False,
+    )
+
+    subparsers = parser.add_subparsers(
+        title="subcommands", help="sub-command help", dest="subcommands"
+    )
+
+    # STOP
+    parser_stop = subparsers.add_parser("stop", help="Stops the current timer")
+    parser_stop.set_defaults(func=stop)
+
+    # START
+    parser_start = subparsers.add_parser("start", help="Start a timer")
+    parser_start.add_argument(
+        "project", help="the name of the project to start", choices=projects
+    )
+    parser_start.set_defaults(
+        func=start, user_data=user_data, projects=projects, tags=tags
+    )
+    # SYNC
+    parser_sync = subparsers.add_parser("sync", help="Syncs user data with Toggl")
+    parser_sync.set_defaults(func=sync)
+
+    args = parser.parse_args()
+    if args.running:
+        show_running_timer(args)
+    elif args.list:
+        for project in projects:
+            print(project)
+    elif args.subcommands is None:
+        parser.print_help()
+    else:
+        args.func(args)
+
+
+if __name__ == "__main__":
+    main()
